55cadded741b4b815804de6b499bde62ca4949f8
Miguel Vazquez <miguel.vazquez@cnio.es>
2016-07-21 12:52:39 +0200

    nil

--- etc/app.d/post.rb
+++ etc/app.d/post.rb
@@ -2,16 +2,7 @@ require 'rbbt/entity/study'
 
 require 'kramdown'
 
-
-def user_studies
-  @user_studies = Hash.new{ Sample.all_studies.sort }
-end
-
-
 require 'rbbt'
-
-#path = Path.setup('', nil, nil, :_local => Sample.study_repo.find["{PATH}"], :global => ICGC.root.find["{PATH}"]) 
-#
 
 module Sinatra
   module RbbtAuth
@@ -24,9 +15,9 @@ module Sinatra
 end
 
 
-require 'rbbt/entity'
-require 'rbbt/sources/organism'
-module Object::Gene
-  extend Entity
-  add_identifiers Organism.identifiers("NAMESPACE"), "Ensembl Gene ID", "Associated Gene Name"
-end
+#require 'rbbt/entity'
+#require 'rbbt/sources/organism'
+#module Object::Gene
+#  extend Entity
+#  add_identifiers Organism.identifiers("NAMESPACE"), "Ensembl Gene ID", "Associated Gene Name"
+#end
--- etc/app.d/pre.rb
+++
@@ -1,5 +0,0 @@
-require 'rbbt/workflow'
-
-Workflow.require_workflow "ICGC"
-Workflow.require_workflow "Study"
-Sample.study_repo = ICGC.root.find
